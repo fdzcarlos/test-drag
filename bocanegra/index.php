e96261b997c83708ed39c3d1083304945725912b
fdzcarlos <32142576+fdzcarlos@users.noreply.github.com>
2017-09-24 13:49:45 -0500

    Add files via upload

---
+++ bocanegra/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="/util/interfase/css/bootstrap4/bootstrap.min.css">
+		<link rel="stylesheet" href="/util/interfase/css/style.css">
+
+		<title>Bocanegra</title>
+
+		<script src="/util/js/jquery-3.2.1.min.js"></script>
+		<script src="/util/js/side_script.js"></script>
+	</head>
+	<body>
+		<div class="content-fluid">
+			<figure>
+				<div class="titulo" id="rightTittle">
+					<p>Titulo 1</p>
+				</div>
+				<div class="descripcion">
+					<p>Descripcion 1</p>
+				</div>
+				<div class="divisor">
+					<div class="inception">
+						<div class="titulo" id="leftTittle">
+							<p>Titulo 2</p>
+						</div>
+						<div class="descripcion">
+							<p>Descripcion 2</p>
+						</div>	
+					</div>
+				</div>
+			</figure>
+		</div>
+	</body>
+</html>
