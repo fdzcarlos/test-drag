1ec5e81bf99ffcb78b7176214a8ecee658fe297e
fdzcarlos <32142576+fdzcarlos@users.noreply.github.com>
2017-09-26 00:03:36 -0500

    Add files via upload

--- bocanegra/index.php
+++ bocanegra/index.php
@@ -2,35 +2,38 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="./util/interfase/css/bootstrap4/bootstrap.min.css">
+		<link rel="stylesheet" href="./util/interfase/css/slick/slick.css">
 		<link rel="stylesheet" href="./util/interfase/css/style.css">
 
 		<title>Bocanegra</title>
 
 		<script src="./util/js/jquery-3.2.1.min.js"></script>
+		<script src="./util/js/slick.min.js"></script>
 		<script src="./util/js/side_script.js"></script>
 	</head>
 	<body>
 		<div class="comparison">
 			<figure>
 				<div class="titulo" id="rightTittle">
-					<p>Titulo 1</p>
+					<span>Titulo 1</span>
 				</div>
 				<div class="descripcion" id="rightDesc">
-					<p>Descripcion 1</p>
+					<span>Descripcion 1</span>
 				</div>
-				<div class="divisor">
+				<div class="divisor" id="divisor">
 					<div class="inDivisor">
-						<div class="inception" style="width: 1920px;">
+						<div class="inception">
 							<div class="titulo" id="leftTittle">
-								<p>Titulo 2</p>
+								<span>Titulo 2</span>
 							</div>
 							<div class="descripcion" id="leftDesc">
-								<p>Descripcion 2</p>
+								<span>Descripcion 2</span>
 							</div>	
 						</div>
 					</div>
 				</div>
 			</figure>
+			<input type="range" min="0" max="100" value="50" id="slider" style="margin:0" oninput="moveDivisor()">
 		</div>
 	</body>
 </html>
